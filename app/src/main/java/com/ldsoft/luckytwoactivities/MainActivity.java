e6e0ad54e9952dbd781d528ac0b71b403deec027
Lucky Dewa Satria <luckydewa123@gmail.com>
2020-09-28 19:47:28 +0700

    Initial commit

---
+++ app/src/main/java/com/ldsoft/luckytwoactivities/MainActivity.java
@@ -0,0 +1,84 @@
+package com.ldsoft.luckytwoactivities;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * The TwoActivities app contains two activities and sends messages (intents) between them.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.ldsoft.luckytwoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize all the view variables.
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
+        mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
+    }
+
+    /**
+     * Handle the onClick for the "Send" button. Gets the value of the main EditText,
+     * creates an intent, and launches the second activity with that intent.
+     *
+     * The return intent from the second activity is onActivityResult().
+     * @param view The view (Button) that was clicked.
+     */
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    /**
+     * Handle the data in the return intent from SecondActivity.
+     *
+     * @param requestCode Code for the SecondActivity request.
+     * @param resultCode Code that comes back from SecondActivity.
+     * @param data Intent data sent back from SecondActivity.
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Test for the right intent reply
+        if (requestCode == TEXT_REQUEST) {
+            // Test to make sure the intent reply result was good.
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                // Make the reply head visible.
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                // Set the reply and make it visible.
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
